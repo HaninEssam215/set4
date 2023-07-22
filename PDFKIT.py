12d0050ba683cb31e3aa938caecb179d9ed2db92
HaninEssam215 <120547345+HaninEssam215@users.noreply.github.com>
2023-07-22 16:20:15 +0300

    Initial commit - reminder page is finished

---
+++ PDFKIT.py
@@ -0,0 +1,15 @@
+import pdfkit
+
+path_to_file = 'reminder.html'
+
+
+options = {
+    'page-size': 'A4',
+    'margin-top': '0',
+    'margin-right': '0',
+    'margin-bottom': '0',
+    'margin-left': '0',
+    'enable-local-file-access': True,
+}
+
+pdfkit.from_file(path_to_file, 'output.pdf', options=options)
