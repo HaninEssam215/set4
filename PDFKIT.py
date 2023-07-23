11e37f1f9fa1bc0460be0495333c4f911f3409c8
Hanin Essam <he106400@gmail.com>
2023-07-23 21:27:25 +0300

    Finished invoice page

--- PDFKIT.py
+++ PDFKIT.py
@@ -1,6 +1,6 @@
 import pdfkit
 
-path_to_file = 'reminder.html'
+path_to_file = 'invoice.html'
 
 
 options = {
@@ -12,4 +12,4 @@ options = {
     'enable-local-file-access': True,
 }
 
-pdfkit.from_file(path_to_file, 'reminder_output.pdf', options=options)
+pdfkit.from_file(path_to_file, 'invoice_output.pdf', options=options)
